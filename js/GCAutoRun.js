fc00e65c56355ae9410a4a982162697c7ef7a090
a874723629 <984723629@qq.com>
2018-05-16 13:40:37 +0800

    first commit

---
+++ js/GCAutoRun.js
@@ -0,0 +1,38 @@
+﻿function GCAutoRun(ImagesName, speed) {
+    $(ImagesName).css({
+        "width": "100%",
+        "position": "absolute",
+        "opacity": "0"
+    })
+    function GCrun() {
+        //轮播图数量
+        var num = $(ImagesName).length;
+        //轮播编号
+        var number = 0;
+        numbercheck = function () {
+            number++;
+            if (number >= num) {
+                number = 0;
+            };
+        }
+        function run() {
+            $(ImagesName).eq(number).stop().animate({
+                width: "120%",
+                left: "-10%",
+                opacity: "1"
+            }, speed).siblings().stop().animate({
+                width: "100%",
+                left: "0",
+                opacity: "0"
+            }, speed);
+        }
+        return run;
+    }
+    var Grun = GCrun();
+    numbercheck();
+    Grun();
+    setInterval(function () {
+        numbercheck();
+        Grun();
+    }, speed);
+}
---
+++ js/ScrollAnimate.js
@@ -0,0 +1,41 @@
+﻿var aniname;
+//被卷去的高度
+var ws;
+//窗口高度
+var whight;
+
+//需要执行动画的元素添加class alla
+//data-aniname=执行动画名
+//页面加载时遍历要执行动画的元素
+function reload() {
+    //获取窗口被卷去的内容
+    ws = $(window).scrollTop();
+    //获取窗口高度
+    whight = $(window).height();
+}
+function alla() {
+    $(".alla").each(function () {
+        //获取data-aniname属性
+        aniname = $(this).data("aniname");
+        if ($(this).offset().top <= whight + ws || $(this).offset().top <= whight) {
+            $(this).addClass("animated" + " " + aniname);
+        }
+    });
+}
+$(function () {
+    reload();
+    alla();
+})
+$(window).resize(function () {
+    reload() 
+    alla();
+}).scroll(function () {
+    reload()
+    alla();
+    })
+////当页面滚动时
+//$(window).scroll(function () {
+//    reload() 
+//    alla();
+
+//})
